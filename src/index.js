aad08a1c55657f45c206981caa91fe31d83fdb4e
RobinLee1404 <robz.lee82@gmail.com>
2025-07-13 18:17:00 +0200

    Inject the forecast HTML from Javascript

--- src/index.js
+++ src/index.js
@@ -61,4 +61,32 @@ searchCity(searchInput.value);
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
+
+function displayForecast() {
+  let days = ["Tues", "Wed", "Thur", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+
+    forecastHtml = 
+    forecastHtml +
+
+`<div class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">🌤️</div>
+            <div class="weather-forecast-temperatures">
+              <div class="weather-forecast-temperature">
+                <strong>15º</strong>
+              </div>
+              <div class="weather-forecast-temperature">9º</div>
+            </div>
+          </div>
+          `;
+});
+
+let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = forecastHtml;
+}
+
 searchCity("Lisbon");
+displayForecast();
